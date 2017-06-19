100ab7c2690fbce6e2141a2dc44e91d3b34913d8
xuejian.xu <jearbilove@yeah.net>
2017-06-19 10:19:46 +0800

    remove es2015 transfer

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 include: path.resolve(__dirname, 'src'),
                 loader: 'babel-loader',
                 options: {
-                    presets: ['es2015', 'react', 'stage-0']
+                    presets: ['react', 'stage-0']
                 }
             },
             {
