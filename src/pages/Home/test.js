064495f0c1e91cab3015c4587c87949fc86cc307
xuejian.xu <jearbilove@yeah.net>
2017-06-19 20:42:31 +0800

    add tests

--- src/pages/Home/test.js
+++ src/pages/Home/test.js
@@ -1,5 +1,42 @@
-/**
- * @fileOverView: #
- * @author: xuejian.xu
- * @date: 2017/6/19.
- */
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+import shallowEqual from './modules/shallowEqual';
+
+describe('Different count of keys :', () => {
+    const a = {
+        name: 'callie'
+    };
+
+    const b = {
+        name: 'callie',
+        age: 18
+    };
+
+    describe(JSON.stringify(a), () => {
+        describe(JSON.stringify(b), () => {
+            it('shallowEqual(a, b) should be false', () => {
+                assert.strictEqual(shallowEqual(a, b), false);
+            });
+        });
+    });
+});
+
+describe('Different in deep object :', () => {
+    const a = {
+        name: 'callie',
+        nickName: ['糖糖']
+    };
+
+    const b = {
+        name: 'callie',
+        nickName: ['糖糖']
+    };
+
+    describe(JSON.stringify(a), () => {
+        describe(JSON.stringify(b), () => {
+            it('shallowEqual(a, b) should be false', () => {
+                assert.strictEqual(shallowEqual(a, b), false);
+            });
+        });
+    });
+});
--- test.js
+++ test.js
@@ -1,5 +1,1 @@
-/**
- * @fileOverView: #
- * @author: xuejian.xu
- * @date: 2017/6/19.
- */
+import './src/pages/Home/test';
