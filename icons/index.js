91ba4c0818db75249f0f6de4494239627c1a0ae4
xuejian.xu <jearbilove@yeah.net>
2017-07-07 10:55:46 +0800

    add iconfont preview

--- icons/index.js
+++ icons/index.js
@@ -5,22 +5,28 @@ const webfontsGenerator = require('webfonts-generator');
 const iconArr = glob.sync(__dirname + '/svg/*.svg');
 const destDir = path.join(__dirname, 'dist');
 
-webfontsGenerator({
-    files: iconArr,
-    types: ['ttf', 'eot', 'woff'],
-    dest: destDir,
-    css: true,
-    cssDest: path.resolve(__dirname, '../src/styles/icon.scss'),
-    cssTemplate: path.join(__dirname, 'template.hbs'),
-    cssFontsUrl: '../../icons/dist',
-    templateOptions: {
-        classPrefix: 'i-',
-        baseSelector: 'icon'
+webfontsGenerator(
+    {
+        files: iconArr,
+        types: ['ttf', 'eot', 'woff'],
+        dest: destDir,
+        css: true,
+        cssDest: path.resolve(__dirname, '../src/styles/icon.scss'),
+        cssTemplate: path.join(__dirname, 'template.hbs'),
+        cssFontsUrl: '../../icons/dist',
+        templateOptions: {
+            classPrefix: 'i-',
+            baseSelector: 'icon'
+        },
+        html: true,
+        htmlDest: destDir + '/preview.html',
+        htmlTemplate: path.join(__dirname, 'preview.hbs')
+    },
+    function(error) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Done!');
+        }
     }
-}, function(error){
-    if(error){
-        console.log(error);
-    }else{
-        console.log('Done!');
-    }
-});
+);
