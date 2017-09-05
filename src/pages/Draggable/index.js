5865e44036b3e28676d8a287ec49b10a9267ffe7
xuejian.xu <jearbilove@yeah.net>
2017-09-05 15:22:32 +0800

    update draggable demo

--- src/pages/Draggable/index.js
+++ src/pages/Draggable/index.js
@@ -1,12 +1,34 @@
+import './style.scss';
 import React, { Component } from 'react';
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
+
+const SortableItem = SortableElement(({ value }) => <div className="tab">{value}</div>);
+const SortableList = SortableContainer(({ items }) =>
+  <div className="tabs">
+    {items.map((item, idx) => <SortableItem key={idx} index={idx} value={item} />)}
+  </div>
+);
 
 class DraggableDemo extends Component {
+  state = {
+    tabs: ['a', 'b', 'c', 'd', 'e', 'f']
+  };
+
+  onSortEndHandler({oldIndex, newIndex}){
+    this.setState({
+      tabs : arrayMove(this.state.tabs, oldIndex, newIndex)
+    });
+  }
+
   render() {
+    const { tabs } = this.state;
+
     return (
-      <div>
-        Draggable demo
+      <div id="Draggable" className="m-page">
+        <div className="m-title">Draggable Demo</div>
+        <SortableList axis="xy" items={tabs} onSortEnd={this.onSortEndHandler.bind(this)} />
       </div>
-    )
+    );
   }
 }
 
