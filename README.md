28655d6f073baaabce82a9ffe31365f62905fb8e
xuejian.xu <jearbilove@yeah.net>
2018-01-04 16:00:24 +0800

    update

--- README.md
+++ README.md
@@ -3,7 +3,30 @@
 Tests and demos.
 Tests and demos.
 
-This is modify by dev2
-This is modify by dev3
-This is modify by dev4
-This is modify by dev4-2
+勇敢去学Vue.js吧
+
+每一位React开发者都应该学习Next.js
+
+开始学习Reason（以及Reason React）然后做个业余项目试试吧。
+
+GraphQL必须列入你的2018待办事宜清单。
+
+跟着Relay Modern学，这是React+GraphQL应用的数据抓取选项之一。
+
+Storybook！Storybook！Storybook！
+
+在一个项目上安装Prettier，让你的代码可读性更强。
+
+在一个React项目上学习使用Jest截屏及Enzyme。
+
+学习Flow（React开发者）或者TypeScript（其他人）。
+
+考虑用Gatsby把Markdown文本转换成静态页面。
+
+用React Native开发一个移动app。
+
+用Electron开发一个桌面app。
+
+玩一下Popmotion创作一个流畅的动画。
+
+把这个资源清单导入为Todoist项目。
--- src/pages/CSS/css.js
+++ src/pages/CSS/css.js
@@ -2,13 +2,35 @@ import './css.scss'
 import React from 'react'
 
 export default () => (
-    <div id="CSS" className="m-page">
-        <div className="cp-1">
+    <iv id="CSS" className="m-page">
+        <iv className="cp-1">
             <h1>Chapter 1</h1>
             {/* <div className="box"></div> */}
             <div className="parent">
                 <textarea />
             </div>
-        </div>
-    </div>
+            <div className="box-2">
+                对于高度由内容撑开的容器，自动收起动画效果，可以使用max-height属性，配合transition实现
+                <br/>
+                fdasf
+                <br/>
+                fsdf
+            </div>
+            <div className="box-3">
+                <div className="fl"></div>
+                离开家管理会计离开家离开家离开家离开家离开家离开家离开家
+            </div>
+            <div className="box-4"><span></span></div>
+            <div className="box-5">
+                <img alt="这是一张图片"/>
+            </div>
+            <div className="box-6">
+                正在加载<dot>...</dot>
+            </div>
+            <div className="box">
+                <a href="">登陆</a>
+                <a href="">注册</a>
+            </div>
+        </iv>
+    </iv>
 )
