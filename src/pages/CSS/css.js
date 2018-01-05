8a9f31435e56a6e04859f86ef4ed312aea2b5a68
xuejian.xu <jearbilove@yeah.net>
2018-01-05 19:04:26 +0800

    update

--- src/pages/CSS/css.js
+++ src/pages/CSS/css.js
@@ -11,18 +11,20 @@ export default () => (
             </div>
             <div className="box-2">
                 对于高度由内容撑开的容器，自动收起动画效果，可以使用max-height属性，配合transition实现
-                <br/>
+                <br />
                 fdasf
-                <br/>
+                <br />
                 fsdf
             </div>
             <div className="box-3">
-                <div className="fl"></div>
+                <div className="fl" />
                 离开家管理会计离开家离开家离开家离开家离开家离开家离开家
             </div>
-            <div className="box-4"><span></span></div>
+            <div className="box-4">
+                <span />
+            </div>
             <div className="box-5">
-                <img alt="这是一张图片"/>
+                <img alt="这是一张图片" />
             </div>
             <div className="box-6">
                 正在加载<dot>...</dot>
@@ -30,6 +32,10 @@ export default () => (
             <div className="box">
                 <a href="">登陆</a>
                 <a href="">注册</a>
+            </div>
+            <div className="box bx6">
+                <i className="icon-menu" />
+                <i className="icon-dot" />
             </div>
         </iv>
     </iv>
