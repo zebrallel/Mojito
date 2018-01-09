8a58cbd6ba92c40115e75cd88309bdbf84422b9e
xuejian.xu <jearbilove@yeah.net>
2018-01-09 13:59:51 +0800

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,9 +6,11 @@
 
 import React from 'react'
 import './style.scss'
+import { Portal, PortalWithState } from 'react-portal'
 
 class Home extends React.Component {
     state = {
+        showProtal: false,
         info: {
             age: 18
         }
@@ -16,6 +18,7 @@ class Home extends React.Component {
     clickHandler = () => {
         this.state.info.age = 20
         this.setState({
+            showProtal: true,
             info: this.state.info
         })
     }
@@ -25,16 +28,26 @@ class Home extends React.Component {
                 <div>Home Page</div>
                 <Person info={this.state.info} />
                 <button onClick={this.clickHandler}>Click</button>
+                <PortalWithState closeOnOutsideClick closeOnEsc>
+                    {({ openPortal, closePortal, isOpen, portal }) => [
+                        <button key="foo" onClick={openPortal}>
+                            Open Portal
+                        </button>,
+                        portal(
+                            <div className="portal">
+                                This is more advanced Portal. It handles its own state. <button onClick={closePortal}>Close me!</button>, hit ESC or click outside of me.
+                            </div>
+                        )
+                    ]}
+                </PortalWithState>
             </div>
         )
     }
 }
 
-class Person extends React.PureComponent{
-    render(){
-        return (
-            <div>age: {this.props.info.age}</div>
-        )
+class Person extends React.PureComponent {
+    render() {
+        return <div>age: {this.props.info.age}</div>
     }
 }
 
