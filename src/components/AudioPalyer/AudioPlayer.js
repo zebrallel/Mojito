d1520a9bd07c43c52fdcb1927992bf7a8c737414
xuejian.xu <jearbilove@yeah.net>
2018-06-13 17:06:23 +0800

    add audio

--- src/components/AudioPalyer/AudioPlayer.js
+++ src/components/AudioPalyer/AudioPlayer.js
@@ -1,19 +1,94 @@
 import './AudioPlayer.scss'
 import React from 'react'
 
-class AudioPlayer extends React.Component{
-    render(){
+const signal = {
+    beforeLoad: 0,
+    loading: 1,
+    playing: 2,
+    pause: 3
+}
+
+class AudioPlayer extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            status: signal.beforeLoad
+        }
+        this.audio = new Audio()
+        this.audio.addEventListener(
+            'canplay',
+            () => {
+                this.audio.play()
+
+                this.setState({
+                    status: signal.playing
+                })
+            },
+            false
+        )
+    }
+    toggle() {
+        const { status } = this.state
+
+        switch (status) {
+            case signal.beforeLoad:
+                this.audio.src = this.props.src
+
+                this.setState({
+                    status: signal.loading
+                })
+
+                break
+            case signal.playing:
+                this.audio.pause()
+
+                this.setState({
+                    status: signal.pause
+                })
+
+                break
+            case signal.pause:
+                this.audio.play()
+
+                this.setState({
+                    status: signal.playing
+                })
+
+                break
+        }
+    }
+    render() {
+        const { status } = this.state
+        const {beforeLoadText = '点击开始', pauseText = '暂停中'} = this.props
+
         return (
             <div className="m-audio-player">
-                <div className="left">
-                    <div className="icon"></div>
+                <div
+                    className="left"
+                    onClick={() => {
+                        this.toggle()
+                    }}
+                >
+                    {status === signal.playing ? <span className="icon-pause" /> : <span className="icon-play" />}
                 </div>
                 <div className="right">
-                    right content
+                    {status === signal.beforeLoad && beforeLoadText}
+                    {status === signal.pause && pauseText}
+                    {status === signal.loading && '加载中...'}
+                    {status === signal.playing && (
+                        <div className="waving">
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                            <span />
+                        </div>
+                    )}
                 </div>
             </div>
         )
     }
 }
 
-export default AudioPlayer 
+export default AudioPlayer
--- src/pages/Audio/Audio.js
+++ src/pages/Audio/Audio.js
@@ -8,7 +8,7 @@ class Audio extends React.Component {
             <div id="Audio">
                 <h1>Audio player test</h1>
                 <div className="example">
-                    <AudioPlayer />
+                    <AudioPlayer beforeLoadText="点击开始" pasueText="暂停中" src="http://srv.test.pajkdc.com/ghibli/voc/T13yhQBjKT1RCvBVdK" />
                 </div>
             </div>
         )
